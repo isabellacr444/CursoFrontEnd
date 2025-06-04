1d918577fda0b7687d92b0d47487abfa73b74321
00001114399279sp@al.educacao.sp.gov.br <000011439279sp@al.educacao.sp.gov.br>
2025-06-04 11:31:39 -0300

    04.06

--- FrameWorks/Angular16/app_rh/src/app/app.module.ts
+++ FrameWorks/Angular16/app_rh/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './templates/header/header.component';
+import { FooterComponent } from './templates/footer/footer.component';
+import { VagasComponent } from './views/vagas/vagas.component';
+import { CurriculosComponent } from './views/curriculos/curriculos.component';
+import { HomeComponent } from './views/home/home.component';
+import { PainelVagasComponent } from './views/painel-vagas/painel-vagas.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    VagasComponent,
+    CurriculosComponent,
+    HomeComponent,
+    PainelVagasComponent
   ],
   imports: [
     BrowserModule,
